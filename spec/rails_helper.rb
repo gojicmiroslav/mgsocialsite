a8ddce470a7283ad1aafb3332be7b51005a99ec0
gojicmiroslav <miroslavy2k@gmail.com>
2015-09-29 00:55:29 +0200

    Fixed another rails helper issue

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -51,10 +51,7 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
-<<<<<<< HEAD
-=======
 
   #Configure Devise
   config.include Devise::TestHelpers, type: :controller
->>>>>>> static-pages
 end
