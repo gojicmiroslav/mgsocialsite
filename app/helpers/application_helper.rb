4c499fad471a9a8c3a7f1cf4e0cc1edf60d0d539
gojicmiroslav <miroslavy2k@gmail.com>
2015-11-09 19:38:42 +0100

    Added a flash helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,15 @@ module ApplicationHelper
 		end
 	end
 
+	def flash_class(type)
+		case type
+  		when 'notice'
+  			'alert alert-success'
+  		when 'alert'
+  			'alert alert-danger'
+  		else	
+  			type
+  		end
+	end
+
 end
